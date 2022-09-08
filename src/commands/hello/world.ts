aaf9cee5aabe07e7f9c857c6fb8570ed291242d0
Mike Donnalley <mdonnalley@salesforce.com>
2022-09-08 12:36:31 -0600

    chore: update examples

--- src/commands/hello/world.ts
+++ src/commands/hello/world.ts
@@ -4,7 +4,7 @@ export default class World extends Command {
   static description = 'Say hello world'
 
   static examples = [
-    `$ oex hello world
+    `<%= config.bin %> <%= command.id %>
 hello world! (./src/commands/hello/world.ts)
 `,
   ]
