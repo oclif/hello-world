2dd1cd8484075fbd69abca4a2fb3b23a412de37c
Mike Donnalley <mdonnalley@salesforce.com>
2023-11-29 09:57:53 -0700

    fix: use local ts-node for dev.js

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node_modules/.bin/ts-node
 // eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
-(async () => {
+;(async () => {
   const oclif = await import('@oclif/core')
   await oclif.execute({development: true, dir: __dirname})
 })()
